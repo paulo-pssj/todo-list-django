5b0362b7853abde6de62867b0fa537c9a3545cde
paulo-pssj <paulinhops99@gmail.com>
2022-01-11 15:40:52 -0300

    mudanças nos templates com bootstrap

--- setup/settings.py
+++ setup/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
     'todolist',
     'accounts',
 ]
@@ -116,11 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/static/')
-]
+STATIC_URL = 'static/'
+
 
 # Login settings
 
